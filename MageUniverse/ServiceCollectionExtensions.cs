7f7c247b7ef6aee3844fa674783b51dea3661460
Zakkery <zakkdiaz@gmail.com>
2025-08-10 02:16:02 -0700

    Refactor universe to use octree for gravity

--- MageUniverse/ServiceCollectionExtensions.cs
+++ MageUniverse/ServiceCollectionExtensions.cs
@@ -1,16 +1,26 @@
-﻿using MageUniverse.Physics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ParticleLib.Modern.Models._3D;
 
-namespace MageUniverse
+namespace MageUniverse;
+
+public static class ServiceCollectionExtensions
 {
-    public static class ServiceCollectionExtensions
+    public static IServiceCollection AddUniverse(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddUniversePhysics(this IServiceCollection services)
-        {
-            services.AddSingleton<IPhysicsEngine, PhysicsEngine>();
-            services.AddSingleton<IUniverse, Universe>();
+        var boundsSection = configuration.GetSection("Universe:Bounds");
+        var min = new Point3D(
+            boundsSection.GetValue<float>("MinX", 0),
+            boundsSection.GetValue<float>("MinY", 0),
+            boundsSection.GetValue<float>("MinZ", 0));
+        var max = new Point3D(
+            boundsSection.GetValue<float>("MaxX", 500),
+            boundsSection.GetValue<float>("MaxY", 500),
+            boundsSection.GetValue<float>("MaxZ", 500));
 
-            return services;
-        }
+        services.AddSingleton<IUniverse>(_ => new Universe(new AAABBB(min, max)));
+
+        return services;
     }
 }
+
--- MageUniverse/Universe.cs
+++ MageUniverse/Universe.cs
@@ -9,16 +9,19 @@ namespace MageUniverse;
 public class Universe : IUniverse
 {
     private readonly List<Particle> _particles = new();
-    private readonly IPhysicsEngine _physicsEngine;
     private readonly Timer _timer;
     private readonly Octree _octree;
     private const float TargetInterval = 1f / 120f; // Target 120 updates per second
     private DateTime _lastUpdateTime;
 
-    public Universe(IPhysicsEngine physicsEngine)
+    // physics constants
+    const float minDistance = 1f;
+    const float maxForce = 1000f;
+    const float neighborRadius = 100f;
+
+    public Universe(AAABBB bounds)
     {
-        _physicsEngine = physicsEngine;
-        _octree = new Octree(new AAABBB(new Point3D(0, 0, 0), new Point3D(500, 500, 500)));
+        _octree = new Octree(bounds);
         _lastUpdateTime = DateTime.UtcNow;
         _timer = new Timer(TimerCallback, null, TimeSpan.Zero, TimeSpan.FromSeconds(TargetInterval));
     }
@@ -59,14 +62,53 @@ public class Universe : IUniverse
 
     public void TimeStep(float deltaTime)
     {
-        _physicsEngine.TimeStep(_particles, _octree, deltaTime);
+        // compute gravitational forces
+        for (int i = 0; i < _particles.Count; i++)
+        {
+            var particle = _particles[i];
+            var neighbors = _octree.GetParticlesInRadius(
+                new Point3D(particle.LocationX, particle.LocationY, particle.LocationZ),
+                neighborRadius);
 
+            foreach (var neighborId in neighbors)
+            {
+                if (neighborId <= i) continue;
+
+                var particle1 = _particles[neighborId];
+                float dx = particle1.LocationX - particle.LocationX;
+                float dy = particle1.LocationY - particle.LocationY;
+                float dz = particle1.LocationZ - particle.LocationZ;
+                float distanceSq = dx * dx + dy * dy + dz * dz;
+                float distance = MathF.Sqrt(distanceSq);
+                if (distance < minDistance) distance = minDistance;
+
+                float force = particle.Mass * particle1.Mass / distanceSq;
+                if (force > maxForce) force = maxForce;
+                float invDist = 1f / distance;
+                float ax = dx * invDist;
+                float ay = dy * invDist;
+                float az = dz * invDist;
+
+                particle.VelocityX += force * ax / particle.Mass * deltaTime;
+                particle.VelocityY += force * ay / particle.Mass * deltaTime;
+                particle.VelocityZ += force * az / particle.Mass * deltaTime;
+
+                particle1.VelocityX -= force * ax / particle1.Mass * deltaTime;
+                particle1.VelocityY -= force * ay / particle1.Mass * deltaTime;
+                particle1.VelocityZ -= force * az / particle1.Mass * deltaTime;
+            }
+        }
+
+        var bounds = _octree.Bounds;
         foreach (var particle in _particles)
         {
-            if (particle.LocationX < 0) particle.LocationX = 0;
-            if (particle.LocationY < 0) particle.LocationY = 0;
-            if (particle.LocationX > 500) particle.LocationX = 500;
-            if (particle.LocationY > 500) particle.LocationY = 500;
+            particle.LocationX += particle.VelocityX * deltaTime;
+            particle.LocationY += particle.VelocityY * deltaTime;
+            particle.LocationZ += particle.VelocityZ * deltaTime;
+
+            particle.LocationX = Math.Clamp(particle.LocationX, bounds.Min.X, bounds.Max.X);
+            particle.LocationY = Math.Clamp(particle.LocationY, bounds.Min.Y, bounds.Max.Y);
+            particle.LocationZ = Math.Clamp(particle.LocationZ, bounds.Min.Z, bounds.Max.Z);
 
             _octree.UpdateParticle(particle.OctreeIndex, new Point3D(particle.LocationX, particle.LocationY, particle.LocationZ));
         }
